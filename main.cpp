280edf0de76f9fa112f3acef8b5727c6686a398a
Turani Timea <turanitimi@gmail.com>
2017-10-18 21:20:25 +0300

    Exercitiul 2

--- main.cpp
+++ main.cpp
@@ -4,14 +4,10 @@ using namespace std;
 
 int main()
 {
-    int v, h;
-    double g;
-    cout << "Inaltimea (cm): ";
-    cin >> h;
-    cout << "Varsta (ani): ";
-    cin >> v;
-    g = 50 + 0.75*(h - 150) + 0.25 * (v - 20);
-    cout << "Greutatea ideala (barbat): " << g << "kg" << endl;
-    cout << "Greutatea ideala (femeie): " << 0.9 * g << "kg" << endl;
+    double mile, km;
+    cout << "Lungimea in mile: ";
+    cin >> mile;
+    km = mile * 1.609344;
+    cout << "Lungimea in Km este de " << km << endl;
     return 0;
 }
